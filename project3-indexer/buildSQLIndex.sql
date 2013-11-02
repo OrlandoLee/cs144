5fb1c921610e11562e89b43386031739b1a7e702
OrlandoLee <zongshengli@ucla.edu>
2013-11-01 19:28:41 -0700

    sql script

--- project3-indexer/buildSQLIndex.sql
+++ project3-indexer/buildSQLIndex.sql
@@ -1,2 +1,1 @@
 alter table item add index(seller),add index(buy_price),add index(first_bid),add index(started),add index(ends);
-alter table bids add index(user_id),add index(item_id);
--- project3-indexer/dropSQLIndex.sql
+++ project3-indexer/dropSQLIndex.sql
@@ -1,2 +1,1 @@
 alter table item DROP INDEX seller,DROP INDEX buy_price,DROP INDEX first_bid,DROP INDEX started,DROP INDEX ends;
-alter table bids DROP INDEX user_id,DROP INDEX item_id;
