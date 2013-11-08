6c2811c1dda30ec19dd46040f1deaca909dad252
OrlandoLee <zongshengli@ucla.edu>
2013-11-07 22:53:20 -0800

    refactoring

--- project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
+++ project3-indexer/src/edu/ucla/cs/cs144/Indexer.java
@@ -17,103 +17,114 @@ public class Indexer {
     /** Creates a new instance of Indexer */
     public Indexer() {
     }
- 
+    private IndexWriter indexWriter = null;
+    
+    public IndexWriter getIndexWriter(boolean create) throws IOException {
+        if (indexWriter == null) {
+            indexWriter = new IndexWriter(System.getenv("LUCENE_INDEX") + "/index1",
+                                          new StandardAnalyzer(),
+                                          create);
+        }
+        return indexWriter;
+    }
+    public void closeIndexWriter() throws IOException {
+        if (indexWriter != null) {
+            indexWriter.close();
+        }
+    }
     public void rebuildIndexes()throws SQLException
 	{
-
+        
         Connection conn = null;
-
+        
         // create a connection to the database to retrieve Items from MySQL
-	try {
-	    conn = DbManager.getConnection(true);
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
-
-	
-	Statement stmt = conn.createStatement();
-	Statement stmt1 = conn.createStatement();
-	
-	String itemId = "";	
-	String name = "";
-	String description = "";
-	String fullSearchableText = "";
-	String category = ""; //from item category table
-	
-	ResultSet rs = stmt.executeQuery("select * from item");
-
-
-
-
-	
-	String index_directory = System.getenv("LUCENE_INDEX");
-try{	
-	IndexWriter indexWriter = new IndexWriter( index_directory + "/index1", new StandardAnalyzer(), true);
-	while(rs.next()){
-		Document doc = new Document();
-		fullSearchableText = "";
-		itemId = rs.getString("item_id");
-		name = rs.getString ("name");
-		description = rs.getString ("description");		
-		fullSearchableText = name + " "+ description+" ";
-	
-		doc.add(new Field("itemId", itemId, Field.Store.YES, Field.Index.NO ));
-		doc.add(new Field("name", name, Field.Store.YES, Field.Index.TOKENIZED ));
-		doc.add(new Field("description", description, Field.Store.YES, Field.Index.TOKENIZED ));
-		doc.add(new Field("content", fullSearchableText, Field.Store.NO, Field.Index.TOKENIZED ));
-		indexWriter.addDocument(doc);	
-   	} 
-
-	ResultSet rs_category = stmt1.executeQuery("select * from id_category");
-
-	while(rs_category.next())
-	{
-		Document doc1 = new Document();
-		itemId = rs_category.getString("item_id");
-		category = rs_category.getString("category");
-		doc1.add(new Field("itemId", itemId, Field.Store.YES, Field.Index.NO ));
-		doc1.add(new Field("category", category, Field.Store.YES, Field.Index.TOKENIZED));
-		indexWriter.addDocument(doc1);
-	}
-	rs_category.close();
-	
- if (indexWriter != null) {
-            indexWriter.close();
-  }
-}catch (Exception e)
-{
-	System.out.println("Exception caught.\n");
-}
-	
-	rs.close();
-	stmt.close();
-	stmt1.close();
-	
-	
-	try {
-	    conn.close();
-	} catch (SQLException ex) {
-	    System.out.println(ex);
-	}
-   
+        try {
+            conn = DbManager.getConnection(true);
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
+        
+        Statement stmt = conn.createStatement();
+        Statement stmt1 = conn.createStatement();
+        
+        String itemId = "";
+        String name = "";
+        String description = "";
+        String fullSearchableText = "";
+        String category = ""; //from item category table
+        
+        ResultSet rs = stmt.executeQuery("select * from item");
+        
+        
+        try{
+            getIndexWriter(true);
+            while(rs.next()){
+                IndexWriter writer = getIndexWriter(false);
+                category = "";
+                Document doc = new Document();
+                fullSearchableText = "";
+                itemId = rs.getString("item_id");
+                name = rs.getString ("name");
+                description = rs.getString ("description");
+                fullSearchableText = name + " "+ description+" ";
+                
+                doc.add(new Field("itemId", itemId, Field.Store.YES, Field.Index.NO ));
+                doc.add(new Field("name", name, Field.Store.YES, Field.Index.TOKENIZED ));
+                doc.add(new Field("description", description, Field.Store.YES, Field.Index.TOKENIZED ));
+                doc.add(new Field("content", fullSearchableText, Field.Store.NO, Field.Index.TOKENIZED ));
+                ResultSet rs_category = stmt1.executeQuery("select * from id_category where item_id ="+ itemId);
+                while(rs_category.next())
+                {
+                    category = category + " " + rs_category.getString("category");
+                    
+                }
+                doc.add(new Field("category", category, Field.Store.YES, Field.Index.TOKENIZED));
+                
+                writer.addDocument(doc);
+            }
+            
+            
+            
+            rs.close();
+            rs_category.close();
+            stmt.close();
+            stmt1.close();
+            
+            closeIndexWriter();
+        }catch (Exception e)
+        {
+            System.out.println("Exception caught.\n");
+        }
+        
+        
+        
+        
+        
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            System.out.println(ex);
+        }
+        
 	}
 	
     public static void main(String args[]){
         Indexer idx = new Indexer();
 		try{
 			idx.rebuildIndexes();
-			}
+        }
 		catch(SQLException ex){
-		            System.out.println("SQLException caught");
-		            System.out.println("---");
-		            while ( ex != null ){
-		                System.out.println("Message   : " + ex.getMessage());
-		                System.out.println("SQLState  : " + ex.getSQLState());
-		                System.out.println("ErrorCode : " + ex.getErrorCode());
-		                System.out.println("---");
-		                ex = ex.getNextException();
-		            }
-		       }
+            System.out.println("SQLException caught");
+            System.out.println("---");
+            while ( ex != null ){
+                System.out.println("Message   : " + ex.getMessage());
+                System.out.println("SQLState  : " + ex.getSQLState());
+                System.out.println("ErrorCode : " + ex.getErrorCode());
+                System.out.println("---");
+                ex = ex.getNextException();
+            }
+        }
 		
     }   
 }
