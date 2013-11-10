c041d324bf1941cdd8a2867635b9c17ccaadb232
OrlandoLee <zongshengli@ucla.edu>
2013-11-09 20:22:22 -0800

    project3c

--- project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
+++ project3-searcher/src/edu/ucla/cs/cs144/AuctionSearch.java
@@ -115,7 +115,7 @@ public class AuctionSearch implements IAuctionSearch {
             
             String mysqlQuery = "";
             String luceneQuery = "";
-	    int bidindex = -1;
+	    	int bidindex = -1;
             for(int i=0; i<constraints.length; i++)
             {
                 String constrainsName = constraints[i].getFieldName();
@@ -205,12 +205,11 @@ public class AuctionSearch implements IAuctionSearch {
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(mysqlQuery);
             
-	//result[j] = new SearchResult(itemId,name);
             while(rs.next())
             {  
                 mysqlSet.add(rs.getString("item_id"));
             	temp_result.put(rs.getString("item_id"),rs.getString("name"));
-	    }
+	    	}
 	}
             if(!mysqlQuery.equals("")&&!luceneQuery.equals("")) 
             luceneSet.retainAll(mysqlSet);
@@ -219,7 +218,6 @@ public class AuctionSearch implements IAuctionSearch {
 	   
             int finalSize = Math.min( (luceneSet.size()-numResultsToSkip), numResultsToReturn);
 	    result = new SearchResult[finalSize];
-	    //operation on set
             Iterator<String> iterator = luceneSet.iterator();
             int index = 0;
 	    int count = 0; 
@@ -231,7 +229,7 @@ public class AuctionSearch implements IAuctionSearch {
 		if (temp_result.get(itemId)!=null)
                   {
                     String name = temp_result.get(itemId);
-		    result[index] =  new SearchResult(itemId, name);
+		    		result[index] =  new SearchResult(itemId, name);
                     index++;
                   }
 		else
@@ -272,8 +270,137 @@ public class AuctionSearch implements IAuctionSearch {
     
 	public String getXMLDataForItemId(String itemId) {
 		// TODO: Your code here!
-		return null;
+		Connection conn = null;
+		String xml=null;
+		try
+		{
+			SimpleDateFormat newformat = new SimpleDateFormat("MMM-dd-yy H:m:s");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:m:s");
+
+            conn = DbManager.getConnection(true);
+            Statement stmt = conn.createStatement();
+			String query = "SELECT * FROM item JOIN user ON item.seller = user.user_id WHERE item_id = " +  "\"" +itemId + "\""; //item & user table
+            ResultSet rs = stmt.executeQuery(query);
+            
+			HashMap<String, String> tag_table = new HashMap<String,String>();
+			tag_table.put("name", "Name");
+			tag_table.put("category","Category");
+			tag_table.put("currently","Currently");
+			tag_table.put("buy_price","Buy_Price");
+			tag_table.put("first_bid","First_Bid");
+			tag_table.put("number_of_bids","Number_of_Bids");
+			tag_table.put("bids","Bids");
+			tag_table.put("bid","Bid");
+			tag_table.put("bidder","Bidder");
+			tag_table.put("location","Location");
+			tag_table.put("country","Country");
+			tag_table.put("time","Time");
+			tag_table.put("amount","Amount");
+			tag_table.put("started","Started");
+			tag_table.put("ends","Ends");
+			tag_table.put("description","Description");
+			//TO-DO FILL OUT THE TABLE
+			
+            while(rs.next())
+            {  
+			   	xml += "<Item ItemID=\"" + itemId + "\">\n";
+				xml += "  " + getXMLLine(tag_table.get("name"), rs);
+				//category 
+				Statement category_stmt = conn.createStatement();
+				String category_query = "Select * from id_category where item_id = " + "\"" + itemId +"\"";
+				ResultSet category_rs = category_stmt.executeQuery(category_query);
+				while(category_rs.next())
+				{
+					xml + = "  " + getXMLLine(tag_table.get("category"), category_rs);
+				}
+				category_rs.close();
+				category_stmt.close();
+				
+				xml += "  " + getXMLLine(tag_table.get("currently"), rs);
+				
+				if (!getXMLLine(tag_table.get("buy_price"), result).equals("<Buy_Price>$0.00</Buy_Price>\n"))
+			         xml += "  " + getXMLLine(tag_table.get("buy_price"), rs); 
+			                               
+			 	xml += "  " + getXMLLine(tag_table.get("first_bid"), rs);                // Parse First Bid tag
+			 	xml += "  " + getXMLLine(tag_table.get("number_of_bids"), rs);   // Parse Number of Bids tag
+			  
+				Statement bids_stmt = conn.createStatement();
+				String bids_query = "SELECT * FROM bids JOIN user ON bids.user_id = user.user_id WHERE item_id = " +  "\"" +itemId + "\"";
+				ResultSet bids_rs = bids_stmt.executeQuery(bids_query);
+				xml += "  " + "<" + tag_table.get("bids") + ">\n";
+				
+				while (bids_result.next()) {
+			                                 	xml += "    <" + tag_table.get("bid") + ">\n";
+			                                    xml += "      <" + tag_table.get("bidder") + " UserID=\"" + bids_rs.getString("user_id") + "\" ";
+			                                    xml += "Rating=\"" + bids_rs.getString("rating") + "\">\n";
+	                                            xml += "        " + getXMLLine(tag_table.get("location"), bids_rs);     // Find Bidder's Location if present
+			                        			xml += "        " + getXMLLine(tag_table.get("country"), bids_rs);      // Find Bidder's Country if present
+			                                    xml += "      </" + tag_table.get("bidder") + ">\n";
+												Date bid_parsed = format.parse(bids_rs.getString("time"));
+										        bid_value = newformat.format(parsed);
+			                                    xml += "      <" + tag_table.get("time") + ">" + bid_value + "</" + tag_table.get("time") + ">\n";
+			                                    xml += "      " + getXMLLine(tag_table.get("amount"), bids_rs);
+			                                    xml += "    </" + tag_table.get("bid") + ">\n";
+			                      
+			                                }
+                 xml += "  </" + tag_table.get("bids") + ">\n";
+			     bids_rs.close();
+		         bids_stmt.close();
+		
+				 xml += "  " + getXMLLine(tag_table.get("location"), rs); 
+				 xml += "  " + getXMLLine(tag_table.get("country"), rs);
+ 				 xml += "  " + getXMLLine(tag_table.get("started"), rs);                  // Parse Started tag
+				 xml += "  " + getXMLLine(tag_table.get("ends"), rs);                             // Parse Ends tag
+  				 xml += "  <" + tag_table.get("tag") + " ";                                                // Parse Seller tag
+				 xml += "UserID" + "=\"" + rs.getString("seller") + "\" ";
+				 xml += "Rating=\"" + rs.getString("rating") + "\"/>\n";
+				 xml += "  " + getXMLLine(tag_table.get("description"), rs);              // Parse Description tag
+ 				 xml += "</" + "Item" + ">";
+	    	}
+				rs.close();
+				stmt.close();
+				conn.close();
+			
+		} catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (CorruptIndexException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } 
+		  catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (java.text.ParseException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	
+		return xml;
 	}
+	
+	private String getXMLLine(String tag, ResultSet r) throws SQLException {
+	                String xml = "<" + tag + ">";
+	                if (tag.equals("Currently") || tag.equals("Buy_Price") || tag.equals("First_Bid")  || tag.equals("Amount"))
+	                        xml += "$";
+	                if (tag.equals("Started") || tag.equals("Ends") || tag.equals("Time"))
+	                       { 
+							 	Date parsed = format.parse(r.getString(tag));
+					            value = newformat.format(parsed);
+								xml += value;
+							}
+	                else
+	                        xml += escape(r.getString(tag));
+	                xml += "</" + tag + ">\n";
+	                return xml;
+	        }
+	private String escape(String s) {
+		                return s.replaceAll("\"", "quot;").replaceAll("\'", "&apos;").replaceAll("&", "&amp;").replaceAll("<", "&lt;").replaceAll(">", "&gt;");
+		        }
+	
 	
 	public String echo(String message) {
 		return message;
