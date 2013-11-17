97f7c6f2a939aff94b6efe0f538164d4cdd785e5
OrlandoLee <zongshengli@ucla.edu>
2013-11-17 09:35:25 -0800

    project4

---
+++ project4/WebContents/search.jsp
@@ -0,0 +1,12 @@
+﻿<html>
+<head>
+	<%@ page import="edu.ucla.cs.cs144.*" %>
+	<% SearchResult[] rs = (SearchResult[])request.getAttribute("results") %>
+</head>
+<body>
+	<%for (SearchResult r:rs) { %>
+	<%= r.getItemId()%> , <%= r.getName()%>
+	<br>
+	<% }%>
+</body>
+</html>
--- project4/src/edu/ucla/cs/cs144/SearchServlet.java
+++ project4/src/edu/ucla/cs/cs144/SearchServlet.java
@@ -13,6 +13,19 @@ public class SearchServlet extends HttpServlet implements Servlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
-        // your codes here
+		String q = request.getAttribute("q");
+		String numResultsToSkip = request.getAttribute("numResultsToSkip");
+		String numResultsToReturn = request.getAttribute("numResultsToReturn");
+		int int_numResultsToSkip = 0;
+		int int_numResultsToReturn = 0;
+		if (numberResultsToSkip != null)
+			int_numResultsToSkip = Integer.valueOf(numberResultsToSkip);
+		if(numberResultsToReturn !=null)
+			int_numResultsToReturn = Integer.valueOf(numberResultsToReturn);
+			
+		SearchResult[] results = AuctionSearchClient.basicSearch(q, int_numResultsToSkip, int_numResultsToReturn);
+        request.setAttribute("results",results);
+		
+		request.getRequestDispatcher("./search.jsp").forward(request, response);
     }
 }
